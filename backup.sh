809b33c995d770ec7e6f57d89f7eea93194dd5bf
Justin M. LaPre <justin@lapre.com>
2026-02-19 01:06:23 -0500

    Add additional excludes to backup.sh

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ DEST=foobar:com-lapre-foobar
 
 rclone sync $SOURCE ${DEST}/curr \
     --fast-list `# useful on bucket-remotes like B2` \
-    --exclude "/docker/**" `# Plus any other excludes` \
+    --exclude "/docker/**" --exclude "/truenas_catalog/**" `# Plus any other excludes` \
     --delete-excluded \
     --links `# translate to .rclonelink - Optional` \
     --progress -v \
