91936eb3ce5ce6ad362720552c78c010416bc032
Justin M. LaPre <justin@lapre.com>
2026-02-19 00:53:33 -0500

    Add backup.sh script for rclone backup to B2

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NOW=$(date  +"%Y-%m-%dT%H%M%S%z")
+
+SOURCE=/mnt/.ix-apps
+DEST=foobar:com-lapre-foobar
+
+rclone sync $SOURCE ${DEST}/curr \
+    --fast-list `# useful on bucket-remotes like B2` \
+    --exclude "/docker/**" `# Plus any other excludes` \
+    --delete-excluded \
+    --links `# translate to .rclonelink - Optional` \
+    --progress -v \
+    --backup-dir ${DEST}/back/"${NOW}" `# IMPORTANT`
